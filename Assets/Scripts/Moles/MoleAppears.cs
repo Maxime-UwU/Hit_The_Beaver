ec148e5de7f5e1ae632cd463a8b9b0e011f0f5b6
Maxime Maxime <71704827+Maxime-UwU@users.noreply.github.com>
2024-11-20 12:08:31 +0100

    finish the moles

--- Assets/Scripts/Moles/MoleAppears.cs
+++ Assets/Scripts/Moles/MoleAppears.cs
@@ -1,50 +1,61 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
 
 public class MoleAppears : MonoBehaviour
 {
+    public GameObject[] MolePrefabs;
+    public GameObject Holes;
+    private bool alreadyMole = false;
 
-    // Start is called before the first frame update
     void Start()
     {
-        RandomMole();
+        StartCoroutine(RandomMole());
     }
 
-    // Update is called once per frame
-    void Update()
+    private IEnumerator RandomMole()
     {
-        
-    }
 
-    private void RandomMole()
-    {
-        GameObject Mole;
-        int moleNumber = Random.Range(0, 20);
-        string moleName;
-        if (moleNumber == 0)
+        if (alreadyMole)
         {
-            moleName = "Mole";
-            Mole = GameObject.Find(moleName);
+            yield break;
         }
-        else
-        {
-            string moleNumberFormatted = moleNumber.ToString("D2");
-            moleName = "Mole.0" + moleNumberFormatted;
-            Mole = GameObject.Find(moleName);
-        }
-        Debug.Log(moleNumber);
-        GameObject.Instantiate(Mole);
-        StartCoroutine(DeleteMole(moleName));
 
-        //Mole.transform.position = new Vector3(Mole.transform.position.x + 10, 10, 10);
+        alreadyMole = true;
+
+        int moleIndex = Random.Range(0, MolePrefabs.Length);
+        int holeIndex = Random.Range(1, 6);
+        GameObject Mole = MolePrefabs[moleIndex];
+        string HoleName = "Hole" + holeIndex;
+        GameObject Hole = GameObject.Find(HoleName);
+
+        Vector3 holePosition = Hole.transform.position;
+        holePosition.y -= 1;
+
+        GameObject newMole = Instantiate(Mole, holePosition, Hole.transform.rotation);
+        holePosition.y += 1;
+        newMole.transform.DOMove(new Vector3(holePosition.x, holePosition.y, holePosition.z), 1);
+        yield return new WaitForSeconds(1.5f);
+
+        StartCoroutine(DeleteMole(newMole));
     }
 
-    private IEnumerator DeleteMole(string mole)
+
+    // a appeler quand le marteau touche une taupe
+    private IEnumerator DeleteMole(GameObject moleInstance)
     {
-        yield return new WaitForSeconds(5f);
-        Debug.Log(mole);
-        GameObject LastMole = GameObject.Find(mole + "(Clone)");
-        Destroy(LastMole);
+
+        if (moleInstance != null)
+        {
+            Vector3 molePosition = moleInstance.transform.position;
+            molePosition.y -= 1;
+            moleInstance.transform.DOMove(new Vector3(molePosition.x, molePosition.y, molePosition.z), 1);
+            yield return new WaitForSeconds(1f);
+            Destroy(moleInstance);
+            alreadyMole = false;
+            StartCoroutine(RandomMole());
+        }
+
+
     }
 }
