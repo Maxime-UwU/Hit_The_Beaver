1f135f59f3a92be00b84abe06781b6f2ddfe14dc
Killian.V <90335033+KillianVendewinkele@users.noreply.github.com>
2024-11-21 23:40:19 +0100

    Spawn and stop moles when game start/stop

--- Assets/Scripts/Moles/MoleAppears.cs
+++ Assets/Scripts/Moles/MoleAppears.cs
@@ -4,47 +4,48 @@ using DG.Tweening;
 
 public class MoleAppears : MonoBehaviour
 {
+    [SerializeField] private GameData gameData;
     public GameObject[] MolePrefabs;
     public GameObject Holes;
     private bool alreadyMole = false;
+    private bool stopSpawning = false; // Variable de contrôle pour arrêter les coroutines
 
-    void Start()
+    public IEnumerator RandomMole()
     {
-        StartCoroutine(RandomMole());
-    }
-
-    private IEnumerator RandomMole()
-    {
-
-        if (alreadyMole)
+        while (gameData.getLives() > 0 && !stopSpawning)
         {
-            yield break;
+            if (alreadyMole)
+            {
+                yield return null;
+                continue;
+            }
+
+            alreadyMole = true;
+
+            int moleIndex = Random.Range(0, MolePrefabs.Length);
+            int holeIndex = Random.Range(1, 12);
+            GameObject Mole = MolePrefabs[moleIndex];
+            string HoleName = "Hole" + holeIndex;
+            GameObject Hole = GameObject.Find(HoleName);
+
+            Vector3 holePosition = Hole.transform.position;
+            holePosition.y -= 1;
+
+            GameObject newMole = Instantiate(Mole, holePosition, Hole.transform.rotation);
+            holePosition.y += 1;
+            newMole.transform.DOMove(new Vector3(holePosition.x, holePosition.y, holePosition.z), 1);
+            yield return new WaitForSeconds(1.5f);
+            StartCoroutine(DeleteMole(newMole));
+            if (gameData.getLives() == 0)
+            {
+                stopSpawning = true; 
+            }
         }
-
-        alreadyMole = true;
-
-        int moleIndex = Random.Range(0, MolePrefabs.Length);
-        int holeIndex = Random.Range(1, 12);
-        GameObject Mole = MolePrefabs[moleIndex];
-        string HoleName = "Hole" + holeIndex;
-        GameObject Hole = GameObject.Find(HoleName);
-
-        Vector3 holePosition = Hole.transform.position;
-        holePosition.y -= 1;
-
-        GameObject newMole = Instantiate(Mole, holePosition, Hole.transform.rotation);
-        holePosition.y += 1;
-        newMole.transform.DOMove(new Vector3(holePosition.x, holePosition.y, holePosition.z), 1);
-        yield return new WaitForSeconds(1.5f);
-
-        StartCoroutine(DeleteMole(newMole));
     }
 
-
-    // a appeler quand le marteau touche une taupe
+    // À appeler lorsqu'une taupe est touchée par le marteau
     private IEnumerator DeleteMole(GameObject moleInstance)
     {
-
         if (moleInstance != null)
         {
             Vector3 molePosition = moleInstance.transform.position;
@@ -53,9 +54,13 @@ public class MoleAppears : MonoBehaviour
             yield return new WaitForSeconds(1f);
             Destroy(moleInstance);
             alreadyMole = false;
-            StartCoroutine(RandomMole());
         }
+    }
 
-
+    // Méthode pour arrêter toutes les coroutines en cas de game over ou autre
+    public void StopAllCoroutinesAndSpawning()
+    {
+        stopSpawning = true;
+        StopAllCoroutines();
     }
 }
